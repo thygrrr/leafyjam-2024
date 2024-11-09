e49044cfbb60c4224b6663f150bbd1bcadff82b4
Tiger Jove <tiger@tiger.blue>
2024-11-09 10:30:50 +0100

    Mushroom grow + scale

--- entities/Mushroom.cs
+++ entities/Mushroom.cs
@@ -6,19 +6,25 @@ namespace leafy.entities;
 [GlobalClass]
 public partial class Mushroom : EntityNode2D
 {
-    [Export] public Vector2 ScaleRange = new(0.9f, 1.1f);
-    [Export] public Vector2 AngleRange = new(-2f, 2f);
+    [Export] public Vector2 GrowTimes = new(4, 6);
+    [Export] public Vector2 Angles = new(-2f, 2f);
+    [Export] public Vector2 Scales = new(0.9f, 1.1f);
 
-    private static readonly FastNoiseLite Noise = new();
     
     private float _seed;
-
+    private float _speed;
     private float _growth;
+
+    private Vector2 _orientation;
 
     private Vector2 scaleGene => new(_seed * 100f, _growth);
     private Vector2 angleGene => new(_seed * 100f, _growth);
     
     private AnimatedSprite2D _sprite;
+    private int _frames;
+
+    
+    private static readonly FastNoiseLite Noise = new();
     static Mushroom()
     {
         Noise.SetNoiseType(FastNoiseLite.NoiseTypeEnum.Simplex);
@@ -28,28 +34,33 @@ public partial class Mushroom : EntityNode2D
         Noise.SetFractalGain(0.5f);
     }
     
-    public override void _Ready()
-    {
-        base._Ready();
-    }
-    
     public override void _EnterTree()
     {
         base._EnterTree();
         
         _sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        _frames = _sprite.GetSpriteFrames().GetFrameCount(_sprite.Animation);
+        
         _seed = Random.Shared.NextSingle();
-
+        _speed = Random.Shared.NextSingle();
+        
+        _orientation = new(Random.Shared.NextSingle() > 0.5f ? 1f : -1f, 1f);
+        
         Entity.Add(this);
         Entity.Add(_sprite);
     }
 
     public void Grow(double delta)
     {
-        _growth += (float) delta;
-        _sprite.Frame = (int) Mathf.Floor(_growth);
-        _sprite.Scale = ScaleRange.MapMinMaxV(Noise.GetNoise2Dv(scaleGene));
-        _sprite.RotationDegrees = AngleRange.MapMinMax(Noise.GetNoise2Dv(angleGene));
+        _growth += (float) delta / GrowTimes.Remap(_speed);
+        
+        var frame = (int)Mathf.Floor(_growth * _frames);
+        if (_sprite.Frame != frame)
+        {
+            _sprite.Frame = frame;
+        }
+        _sprite.Scale = Scales.Remap(Noise.GetNoise2Dv(scaleGene)) * _orientation;
+        _sprite.RotationDegrees = Angles.Remap(Noise.GetNoise2Dv(angleGene));
     }
     
     public override void _ExitTree()
