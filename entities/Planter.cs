a3b59ceefa07aba3ae71d72df8525ab2aeb28055
Tiger Jove <tiger@tiger.blue>
2024-11-09 12:29:16 +0100

    Crude trait selection for planter

--- entities/Planter.cs
+++ entities/Planter.cs
@@ -1,15 +1,19 @@
 using System;
+using System.Diagnostics;
 using Godot;
 
 namespace leafy.entities;
 
 public partial class Planter : Control
 {
+    private AnimatedSprite2D _sprite;
+    
     private Ecosystem _ecosystem;
     public override void _Ready()
     {
         base._Ready();
         _ecosystem = GetParent<Ecosystem>();
+        _sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
     }
     
     public override void _Input(InputEvent input)
@@ -21,6 +25,14 @@ public partial class Planter : Control
             if (_ecosystem.ClosestMaturePair(mouseMotion.Position, out var pair))
             {
                 var traits = pair.first.traits | pair.second?.traits;
+                
+                _sprite.Animation = traits switch
+                {
+                    ShroomTraits.Trait1 => "hint_honeymash",
+                    ShroomTraits.Trait2 => "hint_toadstool",
+                    ShroomTraits.Trait3 => "hint_porcini",
+                    _ => "default",
+                };
             }
         }
         
