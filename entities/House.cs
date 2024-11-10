e7b9ab93ab0f3cd1ae7786a8499e9bc50dd3a56f
Tiger Jove <tiger@tiger.blue>
2024-11-10 02:37:51 +0100

    house distance fix

--- entities/House.cs
+++ entities/House.cs
@@ -5,6 +5,6 @@ public partial class House : EntityNode2D
     public override void _Ready()
     {
         Entity.Add(this);
-        Entity.Add(new Position(Position.X, Position.Y));
+        Entity.Add(new Position(GlobalPosition.X, GlobalPosition.Y));
     }
 }
