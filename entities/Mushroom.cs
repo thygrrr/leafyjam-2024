0152605417da6af4c42e33578f08ece04f54b238
Tiger Jove <tiger@jupiter.blue>
2024-11-13 14:54:32 +0100

    improved picking

--- entities/Mushroom.cs
+++ entities/Mushroom.cs
@@ -7,244 +7,244 @@ namespace leafy.entities;
 [GlobalClass]
 public partial class Mushroom : EntityNode2D
 {
-    private Node2D _navTarget;
+	private Node2D _navTarget;
 
-    [Export]
-    private Vector2 _jitterRange = new(-2f, 2f);
-    
-    [Export]
-    private Vector2 _lumiRange = new(0.9f, 1.0f);
-    [Export]
-    private Vector2 _satuRange = new(0.0f, 0.1f);
-    [Export]
-    private Vector2 _hueRange = new(0f, 1.0f);
-    
-    
-    [Export]
-    private Vector2 _growTimes = new(4, 6);
-    [Export]
-    private Vector2 _angles = new(-3f, 3f);
-    [Export]
-    private Vector2 _scales = new(0.9f, 1.1f);
+	[Export]
+	private Vector2 _jitterRange = new(-2f, 2f);
+	
+	[Export]
+	private Vector2 _lumiRange = new(0.9f, 1.0f);
+	[Export]
+	private Vector2 _satuRange = new(0.0f, 0.1f);
+	[Export]
+	private Vector2 _hueRange = new(0f, 1.0f);
+	
+	
+	[Export]
+	private Vector2 _growTimes = new(4, 6);
+	[Export]
+	private Vector2 _angles = new(-3f, 3f);
+	[Export]
+	private Vector2 _scales = new(0.9f, 1.1f);
 
-    [Export]
-    private Vector2 _pitchRange = new(0.9f, 1.1f);
+	[Export]
+	private Vector2 _pitchRange = new(0.9f, 1.1f);
 
-    [Export]
-    public Vector2 plantRange = new(50, 100);
+	[Export]
+	public Vector2 plantRange = new(50, 100);
 
-    [Export]
-    public float pickRange = 20;
+	[Export]
+	public float pickRange = 30;
 
-    [Export(PropertyHint.Enum)]
-    public ShroomTraits traits = default;
-    public ShroomTraits fusion;
+	[Export(PropertyHint.Enum)]
+	public ShroomTraits traits = default;
+	public ShroomTraits fusion;
 
-    private float _seed;
-    private float _speed;
-    private float _growth;
+	private float _seed;
+	private float _speed;
+	private float _growth;
 
-    private Vector2 _orientation;
+	private Vector2 _orientation;
 
-    private Vector2 scaleGene => new(_seed * 100f, _growth);
-    private Vector2 angleGene => new(_seed * 100f, _growth);
+	private Vector2 scaleGene => new(_seed * 100f, _growth);
+	private Vector2 angleGene => new(_seed * 100f, _growth);
 
-    private AnimatedSprite2D _sprite;
-    private int _frames;
+	private AnimatedSprite2D _sprite;
+	private int _frames;
 
-    private static Dictionary<ShroomTraits, (Vector4 pulse, float glow, float amplitude, Color color1, Color color2)> _traitColors;
-    //private static Dictionary<ShroomTraits, Color> _traitColors = new();
-    //private static Dictionary<ShroomTraits, float> _traitGlows = new();
-    
-    private static readonly FastNoiseLite Noise = new();
-    static Mushroom()
-    {
-        Noise.SetNoiseType(FastNoiseLite.NoiseTypeEnum.Simplex);
-        Noise.SetFrequency(.5f);
-        Noise.SetFractalOctaves(3);
-        Noise.SetFractalLacunarity(2.0f);
-        Noise.SetFractalGain(0.5f);
-        
-        _traitColors = new()
-        {
-            #region Basic
-            {
-                ShroomTraits.Toad, 
-                (
-                    new(0, 0, 0, 0), 
-                    0.0f, 0.0f, 
-                    Color.FromHtml("#CEC8B6"), Color.FromHtml("#913426")
-                )
-            },
-            {
-                ShroomTraits.Honey, 
-                (
-                    new(0, 0, 0, 0), 
-                    0.0f, 0.0f, 
-                    Color.FromHtml("#B86D30"), Color.FromHtml("#00ff00")
-                )
-            },
-            {
-                ShroomTraits.Porc, 
-                (
-                    new(0, 0, 0, 0), 
-                    0.0f, 0.0f, 
-                    Color.FromHtml("#977743"), Color.FromHtml("#5F412C")
-                )
-            },
-            #endregion
-            
-            #region 2-Traits
-            {ShroomTraits.Toad | ShroomTraits.Honey, 
-                (
-                    new(7, 4, 7, 7), 
-                    1.5f,  0.3f, 
-                    Color.FromHtml("#D95763"), Color.FromHtml("#D95763")
-                )
-            },
-            {ShroomTraits.Toad | ShroomTraits.Porc,                 (
-                    new(5, 4, 6, 9), 
-                    1.5f, 0.3f, 
-                    Color.FromHtml("#5B6EE1"), Color.FromHtml("#306082")
-                )
-            },
+	private static Dictionary<ShroomTraits, (Vector4 pulse, float glow, float amplitude, Color color1, Color color2)> _traitColors;
+	//private static Dictionary<ShroomTraits, Color> _traitColors = new();
+	//private static Dictionary<ShroomTraits, float> _traitGlows = new();
+	
+	private static readonly FastNoiseLite Noise = new();
+	static Mushroom()
+	{
+		Noise.SetNoiseType(FastNoiseLite.NoiseTypeEnum.Simplex);
+		Noise.SetFrequency(.5f);
+		Noise.SetFractalOctaves(3);
+		Noise.SetFractalLacunarity(2.0f);
+		Noise.SetFractalGain(0.5f);
+		
+		_traitColors = new()
+		{
+			#region Basic
+			{
+				ShroomTraits.Toad, 
+				(
+					new(0, 0, 0, 0), 
+					0.0f, 0.0f, 
+					Color.FromHtml("#CEC8B6"), Color.FromHtml("#913426")
+				)
+			},
+			{
+				ShroomTraits.Honey, 
+				(
+					new(0, 0, 0, 0), 
+					0.0f, 0.0f, 
+					Color.FromHtml("#B86D30"), Color.FromHtml("#00ff00")
+				)
+			},
+			{
+				ShroomTraits.Porc, 
+				(
+					new(0, 0, 0, 0), 
+					0.0f, 0.0f, 
+					Color.FromHtml("#977743"), Color.FromHtml("#5F412C")
+				)
+			},
+			#endregion
+			
+			#region 2-Traits
+			{ShroomTraits.Toad | ShroomTraits.Honey, 
+				(
+					new(7, 4, 7, 7), 
+					1.5f,  0.3f, 
+					Color.FromHtml("#D95763"), Color.FromHtml("#D95763")
+				)
+			},
+			{ShroomTraits.Toad | ShroomTraits.Porc,                 (
+					new(5, 4, 6, 9), 
+					1.5f, 0.3f, 
+					Color.FromHtml("#5B6EE1"), Color.FromHtml("#306082")
+				)
+			},
 
-            {ShroomTraits.Honey | ShroomTraits.Porc,                 (
-                    new(4, 7, 12, 12), 
-                    1.5f, 0.3f, 
-                    Color.FromHtml("#FBF236"), Color.FromHtml("#DF7126")
-                )
-            },
-            
-            #endregion
+			{ShroomTraits.Honey | ShroomTraits.Porc,                 (
+					new(4, 7, 12, 12), 
+					1.5f, 0.3f, 
+					Color.FromHtml("#FBF236"), Color.FromHtml("#DF7126")
+				)
+			},
+			
+			#endregion
 
-            {ShroomTraits.Porc | ShroomTraits.Toad | ShroomTraits.Honey,                 (
-                    new(3, 4, 2, 5), 
-                    -0.5f, 0.5f, 
-                    Color.FromHtml("#ffff00"), Color.FromHtml("#00ffff")
-                )
-            },
-        };
-    }
+			{ShroomTraits.Porc | ShroomTraits.Toad | ShroomTraits.Honey,                 (
+					new(3, 4, 2, 5), 
+					-0.5f, 0.5f, 
+					Color.FromHtml("#ffff00"), Color.FromHtml("#00ffff")
+				)
+			},
+		};
+	}
 
-    public override void _Ready()
-    {
-        base._Ready();
-    
-        _navTarget = GetNode<Node2D>("NavigationTarget");
-        _navTarget.Connect("start_growing", Callable.From(StartGrowing));
-        
-        _sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	public override void _Ready()
+	{
+		base._Ready();
+	
+		_navTarget = GetNode<Node2D>("NavigationTarget");
+		_navTarget.Connect("start_growing", Callable.From(StartGrowing));
+		
+		_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
-        _sprite.Modulate = Color.FromOkHsl(
-            _hueRange.Remap(Random.Shared.NextSingle()),
-            _satuRange.Remap(Random.Shared.NextSingle()), 
-            _lumiRange.Remap(Random.Shared.NextSingle()));
+		_sprite.Modulate = Color.FromOkHsl(
+			_hueRange.Remap(Random.Shared.NextSingle()),
+			_satuRange.Remap(Random.Shared.NextSingle()), 
+			_lumiRange.Remap(Random.Shared.NextSingle()));
 
-        _sprite.Position = new(
-            _jitterRange.Remap(Random.Shared.NextSingle()), 
-            _jitterRange.Remap(Random.Shared.NextSingle())
-        );
+		_sprite.Position = new(
+			_jitterRange.Remap(Random.Shared.NextSingle()), 
+			_jitterRange.Remap(Random.Shared.NextSingle())
+		);
 
-        _frames = _sprite.GetSpriteFrames().GetFrameCount(_sprite.Animation);
+		_frames = _sprite.GetSpriteFrames().GetFrameCount(_sprite.Animation);
 
-        _seed = Random.Shared.NextSingle();
-        _speed = Random.Shared.NextSingle();
+		_seed = Random.Shared.NextSingle();
+		_speed = Random.Shared.NextSingle();
 
-        _orientation = new(Random.Shared.NextSingle() > 0.5f ? 1f : -1f, 1f);
+		_orientation = new(Random.Shared.NextSingle() > 0.5f ? 1f : -1f, 1f);
 
-        Entity.Add(_sprite);
+		Entity.Add(_sprite);
 
-        //NOTE: This is something fennecs should do better, one of the overloads is broken in 0.5.x
-        Entity.Add(new Position(Position.X, Position.Y));
-        
-        Grow(0);
-        
-        InitializeTraits();
-    }
+		//NOTE: This is something fennecs should do better, one of the overloads is broken in 0.5.x
+		Entity.Add(new Position(Position.X, Position.Y));
+		
+		Grow(0);
+		
+		InitializeTraits();
+	}
 
-    private void InitializeTraits()
-    {
-        //GD.Print($"Traits: {traits}, Fusion: {fusion}");
+	private void InitializeTraits()
+	{
+		//GD.Print($"Traits: {traits}, Fusion: {fusion}");
 
-        fusion |= traits;
-        
-        // Select animation if procedural
-        if (traits != fusion)
-        {
-            var shaderParams = _traitColors[fusion];
-            var material = (ShaderMaterial)_sprite.Material.Duplicate();
-            material.SetShaderParameter("color_1", shaderParams.color1);
-            material.SetShaderParameter("color_2", shaderParams.color2);
+		fusion |= traits;
+		
+		// Select animation if procedural
+		if (traits != fusion)
+		{
+			var shaderParams = _traitColors[fusion];
+			var material = (ShaderMaterial)_sprite.Material.Duplicate();
+			material.SetShaderParameter("color_1", shaderParams.color1);
+			material.SetShaderParameter("color_2", shaderParams.color2);
 
-            // Phase shift the pulse
-            var pulse = shaderParams.pulse;
-            pulse.Z *= Random.Shared.NextSingle();
-            pulse.W *= Random.Shared.NextSingle();
-            
-            material.SetShaderParameter("pulse", pulse);
-            material.SetShaderParameter("glow", shaderParams.glow);
-            material.SetShaderParameter("amplitude", shaderParams.amplitude);
-            _sprite.Material = material;
-            
-            switch (traits)
-            {
-                case ShroomTraits.Toad:
-                    _sprite.Animation = "hybrid_toadstool";
-                    break;
-                case ShroomTraits.Honey:
-                    _sprite.Animation = "hybrid_honeymash";
-                    break;
-                case ShroomTraits.Porc:
-                    _sprite.Animation = "hybrid_porcini";
-                    break;
-            }
-        }
-    }
+			// Phase shift the pulse
+			var pulse = shaderParams.pulse;
+			pulse.Z *= Random.Shared.NextSingle();
+			pulse.W *= Random.Shared.NextSingle();
+			
+			material.SetShaderParameter("pulse", pulse);
+			material.SetShaderParameter("glow", shaderParams.glow);
+			material.SetShaderParameter("amplitude", shaderParams.amplitude);
+			_sprite.Material = material;
+			
+			switch (traits)
+			{
+				case ShroomTraits.Toad:
+					_sprite.Animation = "hybrid_toadstool";
+					break;
+				case ShroomTraits.Honey:
+					_sprite.Animation = "hybrid_honeymash";
+					break;
+				case ShroomTraits.Porc:
+					_sprite.Animation = "hybrid_porcini";
+					break;
+			}
+		}
+	}
 
-    public void Grow(float dt)
-    {
-        _growth += dt / _growTimes.Remap(_speed);
+	public void Grow(float dt)
+	{
+		_growth += dt / _growTimes.Remap(_speed);
 
-        if (_growth > 1f)
-        {
-            Entity.Remove<Growing>();
-        }
+		if (_growth > 1f)
+		{
+			Entity.Remove<Growing>();
+		}
 
-        var frame = Mathf.RoundToInt(_growth * _frames);
-        if (_sprite.Frame != frame)
-        {
-            _sprite.Frame = frame;
-        }
-        _sprite.Scale = _scales.Remap(Noise.GetNoise2Dv(scaleGene)) * _orientation;
-        _sprite.RotationDegrees = _angles.Remap(Noise.GetNoise2Dv(angleGene));
-    }
+		var frame = Mathf.RoundToInt(_growth * _frames);
+		if (_sprite.Frame != frame)
+		{
+			_sprite.Frame = frame;
+		}
+		_sprite.Scale = _scales.Remap(Noise.GetNoise2Dv(scaleGene)) * _orientation;
+		_sprite.RotationDegrees = _angles.Remap(Noise.GetNoise2Dv(angleGene));
+	}
 
-    public void Mature()
-    {
-        if (_growth > 0.7f)
-        {
-            Entity.Add<Mature>();
-        }
-    }
+	public void Mature()
+	{
+		if (_growth > 0.7f)
+		{
+			Entity.Add<Mature>();
+		}
+	}
 
-    public void Idle(float dt)
-    {
-        _growth += dt / _growTimes.Remap(_speed) * 0.5f;
-        _sprite.RotationDegrees = _angles.Remap(Noise.GetNoise2Dv(angleGene)); 
-    }
+	public void Idle(float dt)
+	{
+		_growth += dt / _growTimes.Remap(_speed) * 0.5f;
+		_sprite.RotationDegrees = _angles.Remap(Noise.GetNoise2Dv(angleGene)); 
+	}
 
-    public override void _EnterTree()
-    {
-        base._EnterTree();
-        Entity.Add(this);
-    }
+	public override void _EnterTree()
+	{
+		base._EnterTree();
+		Entity.Add(this);
+	}
 
-    private void StartGrowing()
-    {
-        Entity.Add<Growing>();
-        var audio = GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D");
-        audio.PitchScale = _pitchRange.Remap(Random.Shared.NextSingle());
-        audio.Play();
-    }
+	private void StartGrowing()
+	{
+		Entity.Add<Growing>();
+		var audio = GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D");
+		audio.PitchScale = _pitchRange.Remap(Random.Shared.NextSingle());
+		audio.Play();
+	}
 }
--- ui/Planter.cs
+++ ui/Planter.cs
@@ -8,120 +8,120 @@ namespace leafy.ui;
 
 public partial class Planter : Node2D
 {
-    [Export]
-    private AudioStream[] _plantableSounds = [];
+	[Export]
+	private AudioStream[] _plantableSounds = [];
 
-    [Export]
-    private AudioStream[] _harvestSounds = [];
+	[Export]
+	private AudioStream[] _harvestSounds = [];
 
-    [Export] private Vector2 _pitchRange = new(0.9f, 1.1f);
+	[Export] private Vector2 _pitchRange = new(0.9f, 1.1f);
 
-    private AnimatedSprite2D _sprite;
+	private AnimatedSprite2D _sprite;
 
-    private Ecosystem _ecosystem;
+	private Ecosystem _ecosystem;
 
-    private State _state;
-    public override void _Ready()
-    {
-        base._Ready();
-        _ecosystem = GetParent<Ecosystem>();
-        _sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-        _sound = GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D");
-    }
+	private State _state;
+	public override void _Ready()
+	{
+		base._Ready();
+		_ecosystem = GetParent<Ecosystem>();
+		_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		_sound = GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D");
+	}
 
 
-    private Mushroom _plantable;
-    private AudioStreamPlayer2D _sound;
+	private Mushroom _plantable;
+	private AudioStreamPlayer2D _sound;
 
-    private ShroomTraits _traits;
-    private ShroomTraits _fusion;
-    
-    public override void _Input(InputEvent input)
-    {
-        if (_plantable is { Deleting: false })
-        {
-            _plantable.Modulate = Colors.White;
-        }
+	private ShroomTraits _traits;
+	private ShroomTraits _fusion;
+	
+	public override void _Input(InputEvent input)
+	{
+		if (_plantable is { Deleting: false })
+		{
+			_plantable.Modulate = Colors.White;
+		}
 
-        _plantable = null;
-        _traits = default;
+		_plantable = null;
+		_traits = default;
 
-        if (input is InputEventMouse mouseMotion)
-        {
-            if (_ecosystem.ClosestPlantPosition(mouseMotion.Position, out var shroom, out var closestPoint))
-            {
-                Position = closestPoint;
-                _traits = shroom.traits;
+		if (input is InputEventMouse mouseMotion)
+		{
+			if (_ecosystem.ClosestPlantPosition(mouseMotion.Position, out var shroom, out var closestPoint))
+			{
+				Position = closestPoint;
+				_traits = shroom.traits;
 
-                _ecosystem.GatherTraits(closestPoint, out _fusion);
-                _sprite.Animation = PickSpriteAnimation(_traits);
+				_ecosystem.GatherTraits(closestPoint, out _fusion);
+				_sprite.Animation = PickSpriteAnimation(_traits);
 
-                _plantable = shroom;
-                _state = State.Planting;
-            }
-            else if (_ecosystem.ClosestHarvestable(mouseMotion.Position, out var mushroom))
-            {
-                _plantable = mushroom;
-                _plantable.Modulate = Colors.Red;
-                _state = State.Harvesting;
-                _sprite.Animation = "default";
-            }
-            else
-            {
-                Position = mouseMotion.Position;
-                _state = State.Idle;
-                _sprite.Animation = "default";
-            }
-        }
+				_plantable = shroom;
+				_state = State.Planting;
+			}
+			else if (_ecosystem.ClosestHarvestable(mouseMotion.Position, out var mushroom))
+			{
+				_plantable = mushroom;
+				_plantable.Modulate = Colors.Red;
+				_state = State.Harvesting;
+				_sprite.Animation = "default";
+			}
+			else
+			{
+				Position = mouseMotion.Position;
+				_state = State.Idle;
+				_sprite.Animation = "default";
+			}
+		}
 
-        if (input is InputEventMouseButton { ButtonIndex: MouseButton.Left } button && button.IsPressed())
-        {
-            switch (_state)
-            {
-                case State.Planting:
-                {
-                    var planted = ResourceLoader.Load<PackedScene>(_plantable?.SceneFilePath).Instantiate<Mushroom>();
-                    planted.Position = Position;
-                    planted.fusion = _fusion;
-                    GetParent().AddChild(planted);
-                    var sound = _plantableSounds[Random.Shared.Next(_plantableSounds.Length)];
-                    _sound.Stream = sound;
-                    _sound.PitchScale = _pitchRange.Remap(Random.Shared.NextSingle());
-                    _sound.Play();
-                    break;
-                }
+		if (input is InputEventMouseButton { ButtonIndex: MouseButton.Left } button && button.IsPressed())
+		{
+			switch (_state)
+			{
+				case State.Planting:
+				{
+					var planted = ResourceLoader.Load<PackedScene>(_plantable?.SceneFilePath).Instantiate<Mushroom>();
+					planted.Position = Position;
+					planted.fusion = _fusion;
+					GetParent().AddChild(planted);
+					var sound = _plantableSounds[Random.Shared.Next(_plantableSounds.Length)];
+					_sound.Stream = sound;
+					_sound.PitchScale = _pitchRange.Remap(Random.Shared.NextSingle());
+					_sound.Play();
+					break;
+				}
 
-                case State.Harvesting:
-                {
-                    var sound = _harvestSounds[Random.Shared.Next(_harvestSounds.Length)];
-                    _sound.Stream = sound;
-                    _sound.PitchScale = _pitchRange.Remap(Random.Shared.NextSingle());
-                    _sound.Play();
+				case State.Harvesting:
+				{
+					var sound = _harvestSounds[Random.Shared.Next(_harvestSounds.Length)];
+					_sound.Stream = sound;
+					_sound.PitchScale = _pitchRange.Remap(Random.Shared.NextSingle());
+					_sound.Play();
 
-                    _plantable?.QueueFree();
-                    break;
-                }
-            }
-            
-            _plantable = null;
-            _state = State.Idle;
-        }
-    }
+					_plantable?.QueueFree();
+					break;
+				}
+			}
+			
+			_plantable = null;
+			_state = State.Idle;
+		}
+	}
 
-    private StringName PickSpriteAnimation(ShroomTraits traits)
-    {
-        // We select the shape based on the traits.
-        if (traits.HasFlag(ShroomTraits.Honey))return "hint_honeymash";
-        if (traits.HasFlag(ShroomTraits.Toad)) return "hint_toadstool";
-        if (traits.HasFlag(ShroomTraits.Porc)) return "hint_porcini";
-        
-        return "default";
-    }
+	private StringName PickSpriteAnimation(ShroomTraits traits)
+	{
+		// We select the shape based on the traits.
+		if (traits.HasFlag(ShroomTraits.Honey))return "hint_honeymash";
+		if (traits.HasFlag(ShroomTraits.Toad)) return "hint_toadstool";
+		if (traits.HasFlag(ShroomTraits.Porc)) return "hint_porcini";
+		
+		return "default";
+	}
 
-    public enum State
-    {
-        Idle,
-        Planting,
-        Harvesting,
-    }
+	public enum State
+	{
+		Idle,
+		Planting,
+		Harvesting,
+	}
 }
